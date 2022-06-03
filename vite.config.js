d973f16b07f7fcf9a0f6e7776775ff4a9ca5e2d2
simonemiar <79079392+simonemiar@users.noreply.github.com>
2022-06-03 06:28:47 +0200

    responsive

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,16 @@
-
-
 const { resolve } = require("path");
-    
+
 module.exports = {
-    base: "./", //set base here or in the build command
-    build: {
+  base: "./", //set base here or in the build command
+  build: {
     rollupOptions: {
-        input: {
+      input: {
         main: resolve(__dirname, "index.html"),
-        projects: resolve(__dirname, "projects.html"),
-        contact: resolve(__dirname, "contact.html"),
-        },
+        // projects: resolve(__dirname, "projects.html"),
+        contact: resolve(__dirname, "colors_animations.html"),
+        contact: resolve(__dirname, "fonts_ui.html"),
+        contact: resolve(__dirname, "responsive_design.html"),
+      },
     },
-    },
+  },
 };
